eb1d2498b7a2c53814d79641118488904bdd7bb3
Govindarajan V <govindarajanv@gmail.com>
2022-02-13 10:15:12 +0000

    pulumi github demo

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import pulumi
 import pulumi_github as github
 
 # Create a GitHub repository
-repository = github.Repository('demo-repo', 
+repository = github.Repository("demo-repo",name="demo-repo", 
                     description="Demo Repository for GitHub",
                     is_template="true",
                     topics=["12345","abcapp","production"],
@@ -26,7 +26,24 @@ codeowners = github.RepositoryFile("codeowners",
     content="dummy",
     commit_message="Added by automation",
     commit_author="Admin Bot",
-    commit_email="admin@example.com",
+    commit_email="admin@repo.com",
     overwrite_on_create=True)
+
+repo_user = github.get_user(username="govindarajanv")
+
+repo_branch_protection = github.BranchProtection("repoBranchProtection",
+    repository_id=repository.node_id,
+    pattern="main",
+    required_status_checks=[github.BranchProtectionRequiredStatusCheckArgs(
+        strict=True
+    )],
+    required_pull_request_reviews=[github.BranchProtectionRequiredPullRequestReviewArgs(
+        require_code_owner_reviews=True,
+        required_approving_review_count=2,
+        dismiss_stale_reviews=True,
+        restrict_dismissals=True
+    )],
+    enforce_admins=True,
+    allows_deletions=True)
 # Export the Name of the repository
 pulumi.export('name', repository.name)
